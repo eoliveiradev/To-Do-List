bbec68011bfdcaced2e41b40b50955091c998fe2
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-18 20:24:50 -0300

    Created toDoList

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,20 @@ import { GlobalStyle } from './styles/global'
 import { Router} from '../Router'
 import { Header } from './components/Header/Header'
 
+interface taskType{
+  isCompleted: boolean;
+  taskDescription: string;
+}
+
 export default function App() {
+  const [toDoList, setToDoList] = useState<taskType[]>([] as taskType[]);
+
+  setToDoList([
+    ...toDoList, 
+    {isCompleted: false, taskDescription: "Testing Testing Testing"},
+    {isCompleted: false, taskDescription: "Debug Debug Debug Debug"}
+  ]);
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <HashRouter>
