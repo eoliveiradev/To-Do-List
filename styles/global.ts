5e69665ae411ef5a1dbadc35a87bf3015befbbeb
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-17 21:20:36 -0300

    Created default theme

---
+++ styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: 0;
+    outline: none;
+    text-decoration: none;
+    list-style-type: none;
+  }
+`
