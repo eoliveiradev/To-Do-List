5c8fe51dbd687759897c612b8e3118ae514190a2
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-18 22:09:48 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import { useEffect, useState } from 'react'
-import { HashRouter} from 'react-router-dom'
+import { createContext, useEffect, useState } from 'react'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/defaultTheme'
 import { GlobalStyle } from './styles/global'
-import { Router} from '../Router'
+import { Router } from '../Router'
 import { Header } from './components/Header/Header'
 
-interface taskType{
+export interface TaskType {
+  id: string;
   isCompleted: boolean;
   taskDescription: string;
 }
 
+interface ToDoListContextType{
+  toDoList: TaskType[];
+  setToDoList: React.Dispatch<React.SetStateAction<TaskType[]>>;
+}
+export const toDoListContext = createContext({} as ToDoListContextType)
+
 export default function App() {
-  const [toDoList, setToDoList] = useState<taskType[]>([] as taskType[]);
+  const [toDoList, setToDoList] = useState<TaskType[]>([] as TaskType[]);
 
   useEffect(() => {
     setToDoList([
-      ...toDoList, 
-      {isCompleted: false, taskDescription: "Testing Testing Testing"},
-      {isCompleted: false, taskDescription: "Debug Debug Debug Debug"}
+      ...toDoList,
+      { id: "1", isCompleted: false, taskDescription: "Testing Testing Testing" },
+      { id: "2", isCompleted: false, taskDescription: "Debug Debug Debug Debug" }
     ]);
   }, [])
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <HashRouter>
-        <GlobalStyle/>
-        <Router />
-      </HashRouter>
+      <toDoListContext.Provider value={{toDoList, setToDoList}}>
+        <HashRouter>
+          <GlobalStyle />
+          <Router />
+        </HashRouter>
+      </toDoListContext.Provider>
     </ThemeProvider>
   )
 }
