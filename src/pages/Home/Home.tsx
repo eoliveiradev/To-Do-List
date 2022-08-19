f61f83fdec3ce5c0a96fa32b1fa3512b47177323
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-19 13:06:50 -0300

    Added TaskCounter and TaskList to Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { toDoListContext } from "../../App";
+import { Header } from "../../components/Header/Header";
+import { Task } from "../../components/Task/Task";
+import { HomeContainer, NoTaskRegistered, TaskList, TaskSection } from "./styles";
+import { ClipboardText } from "phosphor-react";
+import { TaskCounter } from "../../components/TaskCounter/TaskCounter";
 
-export function Home(){
-  return(
-    <>
-      
-    </>
+export function Home() {
+  const { toDoList } = useContext(toDoListContext);
+  return (
+    <HomeContainer>
+      <TaskSection>
+        <TaskCounter />
+        <TaskList>
+          {toDoList.length > 0 ? (
+            toDoList.map(task => (
+              <Task task={task} />
+            ))
+          ) : (
+            <>
+              <hr/>
+              <NoTaskRegistered>
+                <ClipboardText size={56} color="#3d3d3d" />
+                <h1>Você ainda não tem tarefas cadastradas</h1>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </NoTaskRegistered>
+            </>
+          )}
+        </TaskList>
+      </TaskSection>
+    </HomeContainer>
   )
 }
