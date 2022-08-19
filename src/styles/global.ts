320ac33be7b755c0e537e3e07512033e5c27aeed
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-18 22:10:28 -0300

    Updated global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,5 +9,25 @@ export const GlobalStyle = createGlobalStyle`
     outline: none;
     text-decoration: none;
     list-style-type: none;
+
+    font-family: ${props => props.theme["fonts"]["Inter"]};
+    font-weight: 400;
+
+    background: none;
+    border: none;
+  }
+
+  button, input{
+    cursor: pointer;
+  }
+
+  &:focus-visible{
+    outline: 1px solid black;
+    outline-offset: 4px;
+    outline-color: white;
+  }
+
+  body{
+    background-color: ${props => props.theme["colors"]["base-colors"]["gray-600"]};
   }
 `
