48928b60119e34e07333562721d050bc972f71c0
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-21 12:11:13 -0300

    Refactored Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,34 +1,10 @@
-import React, { useContext } from "react";
-import { toDoListContext } from "../../App";
-import { Header } from "../../components/Header/Header";
-import { Task } from "../../components/Task/Task";
-import { HomeContainer, NoTaskRegistered, TaskList, TaskSection } from "./styles";
-import { ClipboardText } from "phosphor-react";
-import { TaskCounter } from "../../components/TaskCounter/TaskCounter";
+import { HomeContainer } from "./styles";
+import { ToDoList } from "./components/ToDoList/ToDoList";
 
 export function Home() {
-  const { toDoList } = useContext(toDoListContext);
   return (
     <HomeContainer>
-      <TaskSection>
-        <TaskCounter />
-        <TaskList>
-          {toDoList.length > 0 ? (
-            toDoList.map(task => (
-              <Task task={task} />
-            ))
-          ) : (
-            <>
-              <hr/>
-              <NoTaskRegistered>
-                <ClipboardText size={56} color="#3d3d3d" />
-                <h1>Você ainda não tem tarefas cadastradas</h1>
-                <p>Crie tarefas e organize seus itens a fazer</p>
-              </NoTaskRegistered>
-            </>
-          )}
-        </TaskList>
-      </TaskSection>
+      <ToDoList />
     </HomeContainer>
   )
 }
