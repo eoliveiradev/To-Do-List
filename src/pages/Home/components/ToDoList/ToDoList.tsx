8142a7ff14066d2e37b60ecfdc4becd02cffffe9
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-21 12:10:37 -0300

    Featured ToDoList.tsx

---
+++ src/pages/Home/components/ToDoList/ToDoList.tsx
@@ -0,0 +1,37 @@
+import { ClipboardText } from "phosphor-react";
+import { useContext } from "react";
+import { TaskType, toDoListContext } from "../../../../App";
+import { CreateTask } from "../../../../components/CreateTask/CreateTask";
+import { Task } from "../../../../components/Task/Task";
+import { TaskCounter } from "../../../../components/TaskCounter/TaskCounter";
+import { NoTaskRegistered, TaskList, ToDoListContainer } from "./styles";
+
+export function ToDoList() {
+  const { toDoList } = useContext(toDoListContext);
+  
+  return (
+    <ToDoListContainer>
+      <CreateTask />
+      <TaskCounter />
+      <TaskList>
+        {toDoList.length > 0 ? (
+          toDoList.map((task, index) => (
+            <Task 
+              key={index}
+              task={task} 
+            />
+          ))
+        ) : (
+          <>
+            <hr />
+            <NoTaskRegistered>
+              <ClipboardText size={56} color="#3d3d3d" />
+              <h1>Você ainda não tem tarefas cadastradas</h1>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </NoTaskRegistered>
+          </>
+        )}
+      </TaskList>
+    </ToDoListContainer>
+  )
+}
