c9c16763d781bf534a34972ba4f496513d2f0ff8
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-22 20:42:00 -0300

    Featured handleToDoListStorage()

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useEffect, useRef, useState } from 'react'
 import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/defaultTheme'
 import { GlobalStyle } from './styles/global'
 import { Router } from '../Router'
-import { Header } from './components/Header/Header'
 
 export interface TaskType {
   id: string;
@@ -21,13 +20,25 @@ export const toDoListContext = createContext({} as ToDoListContextType)
 export default function App() {
   const [toDoList, setToDoList] = useState<TaskType[]>([] as TaskType[]);
 
+  const getDB = (DB: string) => JSON.parse(localStorage.getItem(DB) as string) ?? [];
+  const setDB = (DBName: string, newDB: any) => localStorage.setItem(DBName, JSON.stringify(newDB));
+
+  const  isToDoListStorageFirstCall = useRef(true)
+  function handleToDoListStorage() {
+    const localDB = getDB("toDoList")
+    if(localDB === null){
+      setDB("toDoList", toDoList)
+    }else if(isToDoListStorageFirstCall.current){
+      setToDoList(localDB)
+      isToDoListStorageFirstCall.current = false
+    }else{
+      setDB("toDoList", toDoList)
+    }
+  }
+
   useEffect(() => {
-    setToDoList([
-      ...toDoList,
-      { id: "1", isCompleted: false, taskDescription: "Testing Testing Testing" },
-      { id: "2", isCompleted: false, taskDescription: "Debug Debug Debug Debug" }
-    ]);
-  }, [])
+    handleToDoListStorage()
+  }, [toDoList])
 
   return (
     <ThemeProvider theme={defaultTheme}>
